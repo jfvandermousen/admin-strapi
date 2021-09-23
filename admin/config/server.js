eca842342ddb9c35c4d5d3c8eb1a47ba2d62c6ad
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-23 13:36:57 +0200

    strapi project

---
+++ admin/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c895fa143e5072bb84ae91af3b56602e'),
+    },
+  },
+});
---
+++ admin/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '144a60ec-6769-4921-9a31-271ce6aff59f'
+};
